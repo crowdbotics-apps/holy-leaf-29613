f7baf09f767e4b1a56e13dc38fbd7831edf543c7
Crowdbotics <team@crowdbotics.com>
2021-08-07 16:07:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "holy_leaf_29613Identifier",
+  url: "https://holy_leaf_29613Identifier.botics.co",
+  api: "https://holy_leaf_29613Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
